37e9c36e91a9e9423d29c1b8080d22a3a87ecc99
Martin <martin_lenart@icloud.com>
2025-08-17 21:57:27 +0200

    Initial commit - cleaned history from branch 4-logger

---
+++ AppWebApi/Program.cs
@@ -0,0 +1,89 @@
+﻿using Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// NOTE: global cors policy needed for JS and React frontends
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//using user secrets
+//You dont need to use the assembly (reflection) to access user secrets at runtime
+//But you need to use the assembly (reflection) to access user secrets at design time (efc migrations)
+//In later branches this code is refactored into a configuration extension that is used both at design time and runtime
+//to switch between user secrets (development) and azure key vault (production)
+var currentDir = Directory.GetCurrentDirectory();
+var assembly = System.Reflection.Assembly.Load("Configuration");
+builder.Configuration.SetBasePath(Path.Combine(currentDir, "../AppWebApi"))
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddUserSecrets(assembly);
+
+// adding options patterns to read appsettings and user secrets
+builder.Services.Configure<AesEncryptionOptions>(
+    options => builder.Configuration.GetSection(AesEncryptionOptions.Position).Bind(options));
+
+builder.Services.Configure<JwtOptions>(
+    options => builder.Configuration.GetSection(JwtOptions.Position).Bind(options));
+
+// adding options and service for multiple Database connections and their respective DbContexts
+builder.Services.Configure<DbConnectionSetsOptions>(
+    options => builder.Configuration.GetSection(DbConnectionSetsOptions.Position).Bind(options));
+builder.Services.AddSingleton<DatabaseConnections>();
+
+// adding encryption
+builder.Services.AddTransient<Encryptions>();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Seido Friends API",
+#if DEBUG
+        Version = "v2.0 DEBUG",
+#else
+        Version = "v2.0",
+#endif
+        Description = "This is an API used in Seido's various software developer training courses."
+        + $"<br>DataSet: {builder.Configuration["DatabaseConnections:UseDataSetWithTag"]}"
+        + $"<br>DefaultDataUser: {builder.Configuration["DatabaseConnections:DefaultDataUser"]}"
+    });
+});
+
+
+//Inject Custom logger, this will also register the InMemoryLoggerProvider logger
+//hence, AddLogging should not be used here
+builder.Services.AddSingleton<ILoggerProvider, InMemoryLoggerProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// for the purpose of this example, we will use Swagger also in production
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seido Friends API v2.0");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseCors(); 
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
