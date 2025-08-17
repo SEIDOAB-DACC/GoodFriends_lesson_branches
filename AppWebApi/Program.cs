f9f337741c581a45fe6b1bf065b500da37359bef
Martin <martin_lenart@icloud.com>
2025-08-17 21:55:09 +0200

    Initial commit - cleaned history from branch 7-dbcontext

---
+++ AppWebApi/Program.cs
@@ -0,0 +1,129 @@
+﻿using Configuration;
+using Configuration.Options;
+using DbContext;
+using DbRepos;
+using Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// NOTE: global cors policy needed for JS and React frontends
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//using user secrets in development
+var currentDir = Directory.GetCurrentDirectory();
+var assembly = System.Reflection.Assembly.Load("Configuration");
+builder.Configuration.SetBasePath(Path.Combine(currentDir, "../AppWebApi"))
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddUserSecrets(assembly);
+
+// adding options patterns to read appsettings and user secrets
+builder.Services.Configure<AesEncryptionOptions>(
+    options => builder.Configuration.GetSection(AesEncryptionOptions.Position).Bind(options));
+
+builder.Services.Configure<JwtOptions>(
+    options => builder.Configuration.GetSection(JwtOptions.Position).Bind(options));
+
+// adding options and service for multiple Database connections and their respective DbContexts
+builder.Services.Configure<DbConnectionSetsOptions>(
+    options => builder.Configuration.GetSection(DbConnectionSetsOptions.Position).Bind(options));
+
+// adding verion info
+builder.Services.Configure<VersionOptions>(options =>VersionOptions.ReadFromAssembly(options));
+
+// Registering database connections service
+builder.Services.AddSingleton<DatabaseConnections>();
+
+// adding DbContexts
+builder.Services.AddDbContext<MainDbContext>((serviceProvider, options) => 
+{ 
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>(); 
+    var databaseConnections = serviceProvider.GetRequiredService<DatabaseConnections>(); 
+    
+    var userRole = configuration["DatabaseConnections:DefaultDataUser"];                      
+    var conn = databaseConnections.GetDataConnectionDetails(userRole);
+    if (databaseConnections.SetupInfo.DataConnectionServer == DatabaseServer.SQLServer)
+    {
+        options.UseSqlServer(conn.DbConnectionString, options => options.EnableRetryOnFailure());
+    }
+    else if (databaseConnections.SetupInfo.DataConnectionServer == DatabaseServer.MySql)
+    {
+        options.UseMySql(conn.DbConnectionString,ServerVersion.AutoDetect(conn.DbConnectionString),
+            b => b.SchemaBehavior(Pomelo.EntityFrameworkCore.MySql.Infrastructure.MySqlSchemaBehavior.Translate, (schema, table) => $"{schema}_{table}"));
+    }
+    else if (databaseConnections.SetupInfo.DataConnectionServer == DatabaseServer.PostgreSql)
+    {
+        options.UseNpgsql(conn.DbConnectionString);
+    }
+    else
+    {
+        //unknown database type
+        throw new InvalidDataException($"DbContext for {databaseConnections.SetupInfo.DataConnectionServer} not existing");
+    }
+});
+
+
+// adding encryption
+builder.Services.AddTransient<Encryptions>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Seido Friends API",
+#if DEBUG
+        Version = "v2.0 DEBUG",
+#else
+        Version = "v2.0",
+#endif
+        Description = "This is an API used in Seido's various software developer training courses."
+        + $"<br>DataSet: {builder.Configuration["DatabaseConnections:UseDataSetWithTag"]}"
+        + $"<br>DefaultDataUser: {builder.Configuration["DatabaseConnections:DefaultDataUser"]}"
+    });
+});
+
+
+
+//Inject Custom logger, this will also register the InMemoryLoggerProvider logger
+//hence, AddLogging should not be used here
+builder.Services.AddSingleton<ILoggerProvider, InMemoryLoggerProvider>();
+
+//Inject DbRepos and Services
+builder.Services.AddScoped<AdminDbRepos>();
+
+builder.Services.AddScoped<IAdminService, AdminServiceDb>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// for the purpose of this example, we will use Swagger also in production
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seido Friends API v2.0");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseCors(); 
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ DbContext/MainDbContext.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+using Configuration;
+using DbModels;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace DbContext;
+
+//DbContext namespace is a fundamental EFC layer of the database context and is
+//used for all Database connection as well as for EFC CodeFirst migration and database updates 
+public class MainDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    IConfiguration _configuration;
+    DatabaseConnections _databaseConnections;
+
+    //used only for the easy demonstration purposes
+    //string _databaseHost = "192.168.68.53"; //used only for databases on remote docker
+    string _databaseHost = "localhost";
+
+
+#if DEBUG
+    // remove password from connection string in debug mode
+    // this is useful for debugging and logging purposes, but should not be used in production code
+    public string dbConnection => System.Text.RegularExpressions.Regex.Replace(
+        this.Database.GetConnectionString() ?? "", @"(pwd|password)=[^;]*;?", "",
+        System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+#endif
+
+    #region C# model of database tables
+    public DbSet<QuoteDbM> Quotes { get; set; }
+    #endregion
+
+    #region constructors
+    public MainDbContext() { }
+    public MainDbContext(DbContextOptions options, IConfiguration configuration, DatabaseConnections databaseConnections) : base(options)
+    { 
+        _databaseConnections = databaseConnections;
+        _configuration = configuration;
+    }
+    #endregion
+
+    //Here we can modify the migration building
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region override modelbuilder
+        #endregion
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    #region DbContext for some popular databases
+    public class SqlServerDbContext : MainDbContext
+    {
+        public SqlServerDbContext() { }
+        public SqlServerDbContext(DbContextOptions options, IConfiguration configuration, DatabaseConnections databaseConnections) 
+            : base(options, configuration, databaseConnections) { }
+
+
+        //Used only for CodeFirst Database Migration and database update commands
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //services are not created by dependency injection when doing a dotnet ef code first migration.
+                //We will create them manually in next branch
+                //Therefore we used literal connect string in this example
+                var connectionString = $"Data Source={_databaseHost},14333;Initial Catalog=sql-friends;Persist Security Info=True;User ID=sa;Pwd=skYhgS@83#aQ;Encrypt=False;";
+                System.Console.WriteLine($"Connection String: {connectionString}");
+
+                optionsBuilder.UseSqlServer(connectionString,options => options.EnableRetryOnFailure());}
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<decimal>().HaveColumnType("money");
+            configurationBuilder.Properties<string>().HaveColumnType("varchar(200)");
+
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Add your own modelling based on done migrations
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class MySqlDbContext : MainDbContext
+    {
+        public MySqlDbContext() { }
+        public MySqlDbContext(DbContextOptions options) : base(options, null, null) { }
+
+
+        //Used only for CodeFirst Database Migration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //services are not created by dependency injection when doing a dotnet ef code first migration.
+                //We will create them manually in next branch
+                //Therefore we used literal connect string in this example
+                var connectionString = $"server={_databaseHost},14333;uid=root;pwd=skYhgS@83#aQ;database=sql-friends;";
+                System.Console.WriteLine($"Connection String: {connectionString}");
+
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+                    b => b.SchemaBehavior(Pomelo.EntityFrameworkCore.MySql.Infrastructure.MySqlSchemaBehavior.Translate, (schema, table) => $"{schema}_{table}"));
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>().HaveColumnType("varchar(200)");
+
+            base.ConfigureConventions(configurationBuilder);
+
+        }
+    }
+
+    public class PostgresDbContext : MainDbContext
+    {
+        public PostgresDbContext() { }
+        public PostgresDbContext(DbContextOptions options) : base(options, null, null){ }
+
+
+        //Used only for CodeFirst Database Migration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //services are not created by dependency injection when doing a dotnet ef code first migration.
+                //We will create them manually in next branch
+                //Therefore we used literal connect string in this example
+                var connectionString = $"Server={_databaseHost};Port=5432;Database=sql-friends;Username=postgres;Password=skYhgS@83#aQ;";
+                System.Console.WriteLine($"Connection String: {connectionString}");
+                
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>().HaveColumnType("varchar(200)");
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+    #endregion
+}
