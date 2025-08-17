765efbe5a22fd44f004f313739cb0f14987546c0
Martin <martin_lenart@icloud.com>
2025-08-17 21:30:34 +0200

    Initial commit - cleaned history from branch 17-jwt-security

---
+++ DbModels/UserDbM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+using Models;
+
+namespace DbModels
+{
+    [Table("Users", Schema = "dbo")]
+    public class UserDbM : User
+	{
+        [Key]     
+        public override Guid UserId { get; set; }
+
+        [Required]
+        public override string UserName { get; set; }
+
+        [Required]
+        public override string Password { get; set; }
+
+        [Required]
+        public override string UserRole { get; set; }
+    }
+}
+
---
+++ Services/LoginService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+
+using DbRepos;
+using Models.DTO;
+using DbContext;
+
+namespace Services;
+
+public class LoginService : ILoginService
+{
+    private readonly LoginDbRepos _repo;
+    private readonly JWTService _jtwService;
+    private readonly ILogger<LoginService> _logger;
+
+    public LoginService(ILogger<LoginService> logger, LoginDbRepos repo, JWTService jtwService)
+    {
+        _repo = repo;
+        _logger = logger;
+        _jtwService = jtwService;
+    }
+
+    public async Task<ResponseItemDto<LoginUserSessionDto>> LoginUserAsync(LoginCredentialsDto usrCreds)
+    {
+        try
+        {
+            var _usrSession = await _repo.LoginUserAsync(usrCreds);
+
+            //Successful login. Create a JWT token
+            _usrSession.Item.JwtToken = _jtwService.CreateJwtUserToken(_usrSession.Item);
+
+            //For test only, decypt the JWT token and compare.
+            var _tmpUserSession = _jtwService.DecodeToken(_usrSession.Item.JwtToken.EncryptedToken);
+
+            return _usrSession;
+        }
+        catch
+        {
+            //if there was an error during login, simply pass it on.
+            throw;
+        }
+    }
+}
+
