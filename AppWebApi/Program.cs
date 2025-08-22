46d17dcf8e3197754f568073efa7f616c90e2ac9
Martin <martin_lenart@icloud.com>
2025-08-22 17:19:09 +0200

    Initial commit - cleaned history from branch 0b-template-swagger

---
+++ AppWebApi/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.AddEndpointsApiExplorer();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Seido Friends API",
+#if DEBUG
+        Version = "v2.0 DEBUG",
+#else
+        Version = "v2.0",
+#endif
+        Description = "This is an API used in Seido's various software developer training courses."
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// for the purpose of this example, we will use Swagger also in production
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seido Friends API v2.0");
+    });
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
